e282514f63b0073f5f4563eb1524f8e3c3fc1291
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2020-12-06 15:38:04 +0000

    juncture-cboe-fx: Fix whitespace

--- libraries/cboe-fx/src/main/java/com/paritytrading/juncture/cboe/fx/itch/package-info.java
+++ libraries/cboe-fx/src/main/java/com/paritytrading/juncture/cboe/fx/itch/package-info.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /**
  * This package contains an implementation of Cboe FX ITCH Protocol 1.65.
  *
